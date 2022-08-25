294b8b786ac31f1bd769da1f1615e82b743cfa72
Yasmin <yasmin.nicolau13@gmail.com>
2022-08-25 15:25:16 -0300

    Iniciando react no projeto blog pessoal

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {   
+    return (
+        <>
+        <h1 className='titulo'>Home</h1>
+        <img src="https://i.imgur.com/4TS42Xa.jpeg" alt="Imagem Tela Inicial" className='img' />
+        </>
+    );
+}
+  export default Home;
